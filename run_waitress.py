529d33be7d493e3aa3b83d02e9de1f19c2545337
Michael Waring <mwaring.mlw@gmail.com>
2025-08-01 03:22:27 -0500

    Changed envoronments and build a clean dependency list Still updating and testing
    calculator.py is a test file.

--- run_waitress.py
+++ run_waitress.py
@@ -2,5 +2,12 @@ from waitress import serve
 import app
 from dotenv import load_dotenv
 
+import sys
+import pprint # Pretty-print, makes the output easier to read
+
+print(f"\n\n--- PYTHON IS SEARCHING THESE PATHS FOR MODULES ---")
+pprint.pprint(sys.path)
+print("----------------------------------------------------\n\n")
+
 serve(app.app, host='0.0.0.0', port=5000, threads=8)
 load_dotenv()
